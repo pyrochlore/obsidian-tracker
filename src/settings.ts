dabee23f62ef4d01fb278644a5168401ece32c58
pyrochlore <pyrochlore@gmail.com>
2021-03-08 14:31:58 +0800

    Edited the text in plugin options page

--- src/settings.ts
+++ src/settings.ts
@@ -2,11 +2,13 @@ import { App, Notice, PluginSettingTab, Setting } from "obsidian";
 import Tracker from './main';
 
 export interface TrackerSettings {
-	mySetting: string;
+	target_folder: string;
+    date_format: string;
 }
 
 export const DEFAULT_SETTINGS: TrackerSettings = {
-	mySetting: 'default'
+	target_folder: "",
+    date_format: ""
 }
 
 export class TrackerSettingTab extends PluginSettingTab {
@@ -22,17 +24,25 @@ export class TrackerSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName("Default folder location")
+			.setDesc("Files in this folder will be parsed and used as input data of the tracker plugin.\nYou can also override it using 'folder' argument int the tracker codeblock.")
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue('')
+				.setPlaceholder("Folder Path")
+				.setValue("")
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.target_folder = value;
+					await this.plugin.saveSettings();
+				}));
+
+        new Setting(containerEl)
+			.setName("Default date format")
+			.setDesc("This format is used to parse the date in your diary title.\nYou can also override it using 'date-format' argument in the tracker codeblock.")
+			.addText(text => text
+				.setPlaceholder("YYYY-MM-DD")
+				.setValue("")
+				.onChange(async (value) => {
+					this.plugin.settings.date_format = value;
 					await this.plugin.saveSettings();
 				}));
 	}
