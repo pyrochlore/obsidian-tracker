19d1950fcc71d12be611f554f87e37273f8acb66
pyrochlore <pyrochlore@gmail.com>
2021-03-16 16:08:07 +0800

    Finished the example_generator

--- examples/example_generator.ts
+++ examples/example_generator.ts
@@ -22,31 +22,47 @@ if (!fs.existsSync(subfolderPath)) {
     fs.mkdirSync(subfolderPath);
 }
 
+let dayCount = 0;
 for (let curDate = startDate.clone(); curDate <= endDate; curDate.add(1, 'days')) {
+    dayCount++;
     let fileName = curDate.format(dateFormat);
     let filePath = path.join(subfolderPath, fileName + ".md");
 
     let fh = fs.openSync(filePath, "w+");
 
+    let content:string = "";
 
     // weight
     let weight = randomFloatFromInterval(60.0, 80.0);
-    let tagWeight = "#weight:" + weight.toString() + "kg";
-    fs.writeFileSync(fh, tagWeight + "\n");
+    let tagWeight = "#weight:" + weight.toFixed(1) + "kg";
+    content += tagWeight + "\n";
 
     // meditation
     let tagMeditation = "#meditation";
-    fs.writeFileSync(fh, tagMeditation + "\n");
+    let missed = randomIntFromInterval(0, 1);
+    if (!missed) {
+        content += tagMeditation + "\n";
+    }
 
     // star
     let textStar = "⭐";
     let numStar = randomIntFromInterval(0, 5);
-    fs.writeFileSync(fh, textStar + "\n");
+    content += textStar.repeat(numStar) + "\n";
 
-    // expense
-    let tagExpense = "#finance/"
+    // finance
+    let tagFinanceBank1 = "#finance/bank1";
+    let tagFinanceBank2 = "#finance/bank2";
 
+    let expense = randomFloatFromInterval(2.0, 3.0);
+    content += tagFinanceBank1 + ":-" + expense.toFixed(1) + "USD" + "\n";
 
+    if (dayCount % 30 == 0) {
+        content += tagFinanceBank2 + ":" + "200USD" + "\n";
+        content += tagFinanceBank2 + "/transfer:" + "-100USD" + "\n";
+        content += tagFinanceBank1 + "/transfer:" + "100USD" + "\n";
+    }
+
+    fs.writeFileSync(fh, content);
     fs.closeSync(fh);
 }
 
