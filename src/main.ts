2382f0624a834641e6787927b14128ad11fa56cf
pyrochlore <pyrochlore@gmail.com>
2021-04-27 10:55:19 +0800

    Fix measuring nested in-line tag

--- src/main.ts
+++ src/main.ts
@@ -387,7 +387,7 @@ export default class Tracker extends Plugin {
 
                     // console.log(content);
                     // Test this in Regex101
-                    // (^|\s)#tagName(:(?<values>[\d\.\/-]*)[a-zA-Z]*)?([\\.!,\\?;~-]*)?(\s|$)
+                    // (^|\s)#tagName(\/[\w-]+)*(:(?<values>[\d\.\/-]*)[a-zA-Z]*)?([\\.!,\\?;~-]*)?(\s|$)
                     let tagName = query.getTarget();
                     if (query.getParentTarget()) {
                         tagName = query.getParentTarget(); // use parent tag name for multiple values
@@ -395,7 +395,7 @@ export default class Tracker extends Plugin {
                     let strHashtagRegex =
                         "(^|\\s)#" +
                         tagName +
-                        "(:(?<values>[\\d\\.\\/-]*)[a-zA-Z]*)?([\\.!,\\?;~-]*)?(\\s|$)";
+                        "(\\/[\\w-]+)*(:(?<values>[\\d\\.\\/-]*)[a-zA-Z]*)?([\\.!,\\?;~-]*)?(\\s|$)";
                     // console.log(strHashtagRegex);
                     let hashTagRegex = new RegExp(strHashtagRegex, "gm");
                     let match;
