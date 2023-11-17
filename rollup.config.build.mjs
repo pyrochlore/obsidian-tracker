54a8b7450805654249208b31502c81280a4422f3
Joe Constant <joe@joeconstant.com>
2023-11-16 19:19:23 -0700

    Update dependencies

--- rollup.config.build.mjs
+++ rollup.config.build.mjs
@@ -1,7 +1,7 @@
 import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 
 export default {
   input: 'src/main.ts',
